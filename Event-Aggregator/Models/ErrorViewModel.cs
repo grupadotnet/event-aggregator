0ede86040322e55064b3c3de1046beb643eb6710
Tomasz Falarz <TheZoooZ@users.noreply.github.com>
2018-12-13 16:00:30 +0100

    Hello World

---
+++ Event-Aggregator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Event_Aggregator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
