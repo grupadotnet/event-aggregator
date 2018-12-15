85661230c33c4a7c24fd319b443534aaaf55c2d6
Tomasz Falarz <t.k.falarz@gmail.com>
2018-12-15 17:04:17 +0100

    Event class added

---
+++ Event-Aggregator/Models/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Event_Aggregator.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Hash { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Localization { get; set; }
+        public string Description { get; set; }
+    }
+}
