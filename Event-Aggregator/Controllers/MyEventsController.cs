0eadced3243b957bac6427523fb491ab62cd8f9d
rozek1szymon <rozek1szymon@gmail.com>
2018-12-21 17:03:58 +0100

    adding possibility to adding events

--- Event-Aggregator/Controllers/MyEventsController.cs
+++ Event-Aggregator/Controllers/MyEventsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Event_Aggregator.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +11,26 @@ namespace Event_Aggregator.Controllers
 {
     public class MyEventsController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private Event_AggregatorContext context;
+        public MyEventsController(Event_AggregatorContext ctx) => context = ctx;
+        [HttpPost]
+        
+        public IActionResult AddEvent(Event eve) 
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                context.Event.Add(eve);
+                context.SaveChanges();
+                ViewBag.Message="added";
+                return View("~/Views/Search/AddEvent.cshtml");
+            }
+            else
+            {
+
+                return View();
+            }
+
+           
         }
     }
 }
