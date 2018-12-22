77d5024ca51c84fb12e01c994f3b5b72ad8fd21e
arkadiusz-cholewa <arkadiusz.cholewaa@gmail.com>
2018-12-22 23:11:43 +0100

    clean up

--- Event-Aggregator/Models/User.cs
+++ Event-Aggregator/Models/User.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Event_Aggregator.Models
+﻿namespace Event_Aggregator.Models
 {
     public class User
     {
