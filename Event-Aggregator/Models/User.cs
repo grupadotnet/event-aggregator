d0bc7d392e1cf04d36093e49c9c6d8a928064712
Szymon Rozek <rozek1szymon@gmail.com>
2019-01-04 14:06:27 +0100

    i will add activation meiling into this and better validation soon

--- Event-Aggregator/Models/User.cs
+++ Event-Aggregator/Models/User.cs
@@ -1,9 +1,18 @@
-﻿namespace Event_Aggregator.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Event_Aggregator.Models
 {
     public class User
     {
         public int ID { get; set; }
         public string Login { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Write phone number down")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Phone number contain of letters only")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Write email down")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
     }
 }
