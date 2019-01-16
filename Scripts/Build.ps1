5c1af2a5967bf2eceb1a3d6cebac698f49393891
Vakcinion <qbeere@gmail.com>
2019-01-16 18:11:35 +0100

    [Jenkins] Changing build solution

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -5,29 +5,27 @@ function buildVS
         [parameter(Mandatory=$true)]
         [String] $path = "C:\Users\biela\Documents\event-aggregator\Event-Aggregator.sln",
 
+        [parameter(Mandatory=$true)]
+        [String] $msBuildExe = "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe",
+
         [parameter(Mandatory=$false)]
         [bool] $nuget = $true,
         
         [parameter(Mandatory=$false)]
         [bool] $clean = $true
     )
-    process
-    {
-        $msBuildExe = 'C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe'
+        #if ($nuget) {
+        #Write-Host "Restoring NuGet packages" -foregroundcolor green
+        #nuget restore "$($path)"
+        #}
 
-        if ($nuget) {
-            Write-Host "Restoring NuGet packages" -foregroundcolor green
-            nuget restore "$($path)"
-        }
-
-        if ($clean) {
-            Write-Host "Cleaning $($path)" -foregroundcolor green
-            & "$($msBuildExe)" "$($path)" /t:Clean /m
-        }
+        #if ($clean) {
+        #    Write-Host "Cleaning $($path)" -foregroundcolor green
+        #    & "$($msBuildExe)" "$($path)" /t:Clean /m
+        #}
 
         Write-Host "Building $($path)" -foregroundcolor green
         & "$($msBuildExe)" "$($path)" /t:Build /m
         Write-Host "Builf finished. :)"
 
-    }
 }
