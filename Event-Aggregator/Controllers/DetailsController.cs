68c0d432a714cc68ebdbd60ea7edd5dcce96ac61
Tomasz Falarz <t.k.falarz@gmail.com>
2019-03-16 20:48:12 +0100

    HomeController's view is passing data to DetailsController now.

--- Event-Aggregator/Controllers/DetailsController.cs
+++ Event-Aggregator/Controllers/DetailsController.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Event_Aggregator.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Event_Aggregator.Controllers
 {
     public class DetailsController : Controller
     {
-        public IActionResult Index()
+        private readonly Event_AggregatorContext _context;
+
+        public DetailsController(Event_AggregatorContext context) => _context = context;
+
+        public IActionResult Index(int id)
         {
+            ViewBag.Id = id;
             return View();
         }
     }
